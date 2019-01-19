516a4d2053f448194714f36963e74aae25683ee1
Steven Pears <steven.pears@live.co.uk>
2019-01-19 22:14:19 +0000

    Add no intent support

---
+++ NottTechMeet_Skill/Handlers/NoThanks.cs
@@ -0,0 +1,19 @@
+﻿using Alexa.NET;
+using Alexa.NET.Request.Type;
+using Alexa.NET.RequestHandlers;
+using Alexa.NET.RequestHandlers.Handlers;
+using Alexa.NET.Response;
+
+namespace NottTechMeet_Skill.Handlers
+{
+    public class NoThanks : IntentNameSynchronousRequestHandler
+    {
+        public NoThanks() : base(BuiltInIntent.No) { }
+
+
+        public override SkillResponse HandleSyncRequest(AlexaRequestInformation information)
+        {
+            return ResponseBuilder.Tell("Okay, no problem");
+        }
+    }
+}
